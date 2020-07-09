43712bbffda139d9393e8585dde8a97cf86ddac0
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-09 15:44:00 +0500

    Creating Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Components/Screens/Home";
+import About from "./Components/Screens/About";
+import Services from "./Components/Screens/Services";
+import Contact from "./Components/Screens/Contact";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Hello From React</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
